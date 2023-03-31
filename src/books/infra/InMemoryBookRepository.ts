79d74ac1115064dc9c842026112a64b5953b38a8
Ezequiel <gomezecu@gmail.com>
2023-03-31 17:54:06 +0200

    refactor(books): en memory repo Map used

--- src/books/infra/InMemoryBookRepository.ts
+++ src/books/infra/InMemoryBookRepository.ts
@@ -2,33 +2,33 @@ import { Book } from '../domain/Book';
 import { BookRepository } from '../domain/BookRepository';
 
 export class InMemoryBookRepository implements BookRepository {
-  private readonly _books: Book[] = [];
+  private _books: Map<string, Book> = new Map();
 
   async findByTitle(title: string): Promise<Book[]> {
-    return this._books.filter((book) => book.title === title);
+    const foundBooks: Book[] = [];
+
+    for (const book of this._books.values()) {
+      if (book.title === title) {
+        foundBooks.push(book);
+      }
+    }
+
+    return foundBooks;
   }
 
   async findById(id: string): Promise<Book | null> {
-    return this._books.find((book) => book.id === id) || null;
+    return this._books.get(id) || null;
   }
 
   async save(book: Book): Promise<void> {
-    const existingBook = this._books.find((b) => b.id === book.id);
-
-    if (!existingBook) {
-      this._books.push(book);
-    }
+    this._books.set(book.id, book);
   }
 
   async delete(book: Book): Promise<void> {
-    const index = this._books.findIndex((b) => b.id === book.id);
-
-    if (index !== -1) {
-      this._books.splice(index, 1);
-    }
+    this._books.delete(book.id);
   }
 
   getAllBooks(): Book[] {
-    return this._books;
+    return [...this._books.values()];
   }
 }
