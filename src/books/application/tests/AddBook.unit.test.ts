f8652fda6f5bb2a8433a7160eb430aef4dfb911b
Ezequiel <gomezecu@gmail.com>
2023-04-01 19:23:18 +0200

    test(books): delete book unit test

--- src/books/application/tests/AddBook.unit.test.ts
+++ src/books/application/tests/AddBook.unit.test.ts
@@ -7,20 +7,17 @@ import { UuidGenerator } from '../../../shared';
 const setup = () => {
   const uuidGenerator = new UuidGenerator();
   const bookFactory = new BookFactory(uuidGenerator);
+  const bookMapper = new BookMapper();
 
-  const bookRepository = {
+  const mockBookRepository = {
     findByTitle: jest.fn(),
     save: jest.fn(),
   } as unknown as BookRepository;
 
-  const bookMapper = {
-    mapBookToBookData: jest.fn(),
-  } as unknown as BookMapper;
-
   return {
     bookFactory,
     bookMapper,
-    bookRepository,
+    mockBookRepository,
   };
 };
 
@@ -28,16 +25,16 @@ describe('AddBook', () => {
   describe('execute', () => {
     it('should throw a DuplicatedBookError if a book with the same title already exists', async () => {
       // Given
-      const { bookFactory, bookMapper, bookRepository } = setup();
+      const { bookFactory, bookMapper, mockBookRepository } = setup();
       const title = 'Test Book';
       const existingBook = { id: '1', title };
 
-      (bookRepository.findByTitle as jest.Mock).mockResolvedValue([
+      (mockBookRepository.findByTitle as jest.Mock).mockResolvedValue([
         existingBook,
       ]);
 
       // When & Then
-      const SUT = new AddBook(bookFactory, bookRepository, bookMapper);
+      const SUT = new AddBook(bookFactory, mockBookRepository, bookMapper);
 
       await expect(SUT.execute({ title })).rejects.toThrow(
         DuplicatedBookError.create(title),
@@ -46,22 +43,18 @@ describe('AddBook', () => {
 
     it('should create a new book and save it to the repository if no book with the same title exists', async () => {
       // Given
-      const { bookFactory, bookMapper, bookRepository } = setup();
+      const { bookFactory, bookMapper, mockBookRepository } = setup();
       const title = 'Test Book';
       const newBook = { _id: expect.anything(), _title: title };
 
-      (bookRepository.findByTitle as jest.Mock).mockResolvedValue([]);
-      (bookMapper.mapBookToBookData as jest.Mock).mockReturnValue({
-        id: newBook._id,
-        title,
-      });
+      (mockBookRepository.findByTitle as jest.Mock).mockResolvedValue([]);
 
       // When
-      const SUT = new AddBook(bookFactory, bookRepository, bookMapper);
+      const SUT = new AddBook(bookFactory, mockBookRepository, bookMapper);
       const result = await SUT.execute({ title });
 
       // Then
-      expect(bookRepository.save).toHaveBeenCalledWith(newBook);
+      expect(mockBookRepository.save).toHaveBeenCalledWith(newBook);
       expect(result).toEqual({ id: newBook._id, title });
     });
   });
---
+++ src/books/application/tests/DeleteBook.unit.test.ts
@@ -0,0 +1,51 @@
+import { DeleteBook } from '../DeleteBook';
+import { BookNotFoundError } from '../../domain/errors';
+
+const setup = () => {
+  const book = { id: '123' };
+  const mockBookRepository = {
+    findById: jest.fn(),
+    delete: jest.fn().mockResolvedValueOnce(undefined),
+    findByTitle: jest.fn(),
+    save: jest.fn(),
+    getAllBooks: jest.fn(),
+  };
+
+  return {
+    book,
+    mockBookRepository,
+  };
+};
+
+describe('DeleteBook', () => {
+  it('should delete a book by the provided book ID', async () => {
+    // Given
+    const { book, mockBookRepository } = setup();
+    const SUT = new DeleteBook(mockBookRepository);
+
+    mockBookRepository.findById.mockResolvedValueOnce(book);
+
+    // When
+    await SUT.execute(book.id);
+
+    // Then
+    expect(mockBookRepository.findById).toHaveBeenCalledWith(book.id);
+    expect(mockBookRepository.delete).toHaveBeenCalledWith(book);
+  });
+
+  it("should throw an error if the book doesn't exist", async () => {
+    // Given
+    const { book, mockBookRepository } = setup();
+    const SUT = new DeleteBook(mockBookRepository);
+
+    mockBookRepository.findById.mockResolvedValueOnce(undefined);
+
+    // When & Then
+    await expect(SUT.execute(book.id)).rejects.toThrow(
+      BookNotFoundError.create(book.id),
+    );
+
+    expect(mockBookRepository.findById).toHaveBeenCalledWith(book.id);
+    expect(mockBookRepository.delete).not.toHaveBeenCalled();
+  });
+});
